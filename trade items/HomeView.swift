b2e9c1b8858519dfebbb200d3da6282e9690d9f0
James Schraut <JT@Schraut.org>
2026-02-23 08:47:23 -0600

    UI Started

---
+++ trade items/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  trade items
+//
+//  Created by JAMES SCHRAUT on 2/23/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
