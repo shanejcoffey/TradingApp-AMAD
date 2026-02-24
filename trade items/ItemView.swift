5f4ced1f3492ee0edaea6f88be1eebcd9d937d14
shanejcoffey <shanejcoffey27@gmail.com>
2026-02-24 07:51:10 -0600

    Item view to get a preview of the item

---
+++ trade items/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 2/24/26.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
