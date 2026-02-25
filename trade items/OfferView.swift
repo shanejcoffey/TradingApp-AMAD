41b4f597cb8b3e6af1cd8abb7ff9024083ff7115
James Schraut <JT@Schraut.org>
2026-02-25 07:40:51 -0600

    UI adjusted

---
+++ trade items/OfferView.swift
@@ -0,0 +1,18 @@
+//
+//  OfferView.swift
+//  trade items
+//
+//  Created by JAMES SCHRAUT on 2/25/26.
+//
+
+import SwiftUI
+
+struct OfferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OfferView()
+}
