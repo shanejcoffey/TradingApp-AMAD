0defb9460197ba49d260d769c315494c06c2488e
James Schraut <JT@Schraut.org>
2026-03-03 07:25:09 -0600

    searchview started

--- trade items/SearchView.swift
+++ trade items/SearchView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var searchIN = ""
+    @State var estValueSearch = 0.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            
+            Text("Search For Items")
+                .font(.largeTitle)
+            TextField("Search", text: $searchIN)
+                .background {
+                    RoundedRectangle(cornerSize: .zero)
+                        .foregroundStyle(.teal)
+                }
+            Text("Filter by:")
+            HStack{
+                Text("Estimated Value: \(estValueSearch, specifier: "%.0f")")
+                Stepper("", value: $estValueSearch)
+            }
+            Slider(value: $estValueSearch, in: 1...100, step: 1.0 ) {
+                
+            }
+                
+                
+
+            
+            
+            
+            
+            
+            
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            NavigationLink {
+                
+            } label: {
+                ZStack{
+                    Circle()
+                        .frame(width: 150)
+                    Text("See results")
+                        .foregroundStyle(.red)
+                        .font(.title2)
+                }
+            }
+            Spacer()
+        }
     }
 }
 
