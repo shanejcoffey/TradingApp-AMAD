d93918e1098b1a42e9672975a6d8e4b9b6913f37
shanejcoffey <shanejcoffey27@gmail.com>
2026-03-06 07:54:39 -0600

    Sprint 2

---
+++ trade items/FilteredItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  FilteredItemsView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 3/5/26.
+//
+
+import SwiftUI
+
+struct FilteredItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilteredItemsView()
+}
