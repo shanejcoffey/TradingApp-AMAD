fdb29cb7f819b7f07cdceb249cb1957acf8e3d56
James Schraut <JT@Schraut.org>
2026-03-11 07:47:36 -0500

    Offer class

---
+++ trade items/Offer.swift
@@ -0,0 +1,29 @@
+//
+//  Offer.swift
+//  trade items
+//
+//  Created by JAMES SCHRAUT on 3/11/26.
+//
+
+import Foundation
+class Offer{
+    
+    var itemTake: Item
+    var itemGive: Item
+    var valueGap = 0
+    
+    init(itemTake: Item, itemGive: Item) {
+        self.itemTake = itemTake
+        self.itemGive = itemGive
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
