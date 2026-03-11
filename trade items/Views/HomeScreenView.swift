3bbfaca7285b669052e401b2b6aa7b575d579c45
shanejcoffey <shanejcoffey27@gmail.com>
2026-03-11 07:49:39 -0500

    tab view

---
+++ trade items/Views/HomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  TabMenuView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 3/11/26.
+//
+
+import SwiftUI
+
+struct TabMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabMenuView()
+}
