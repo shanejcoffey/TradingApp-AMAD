7fea08e222d061a9b23f35de56679161ca9c6b2c
shanejcoffey <shanejcoffey27@gmail.com>
2026-03-12 07:32:48 -0500

    fix navigation

---
+++ trade items/Views/OffersListView.swift
@@ -0,0 +1,18 @@
+//
+//  OffersListView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 3/11/26.
+//
+
+import SwiftUI
+
+struct OffersListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OffersListView()
+}
