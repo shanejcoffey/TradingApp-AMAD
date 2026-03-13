9f3e50aa812c8213e09090d511e8c2bbcb8d5487
shanejcoffey <shanejcoffey27@gmail.com>
2026-03-13 07:33:03 -0500

    offers

---
+++ trade items/Views/OffersView.swift
@@ -0,0 +1,18 @@
+//
+//  OffersView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 3/13/26.
+//
+
+import SwiftUI
+
+struct OffersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OffersView()
+}
---
+++ trade items/Views/SelectOtherItemView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectOtherItemView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 3/13/26.
+//
+
+import SwiftUI
+
+struct SelectOtherItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectOtherItemView()
+}
