a2cb313951cd0c675a3ac2592aa4e20a7e5f9c43
shanejcoffey <shanejcoffey27@gmail.com>
2026-04-22 08:10:47 -0500

    moved signout to new settings screen

---
+++ trade items/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  trade items
+//
+//  Created by SHANE COFFEY on 4/22/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
